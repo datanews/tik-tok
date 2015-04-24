5f6714258351654775ec50598870663fa49c9185
Alan Palazzolo <apalazzolo@minnpost.com>
2015-04-24 14:14:37 -0500

    Adding header into build process.

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,17 @@ var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var plumber = require('gulp-plumber');
 var util = require('gulp-util');
+var header = require('gulp-header');
+
+// Create banner to insert into files
+var pkg = require('./package.json');
+var banner = ['/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
 
 // Plumber allows for better error handling and makes it so that
 // gulp doesn't crash so hard
@@ -31,13 +42,14 @@ gulp.task('support-js', function() {
 });
 
 // Main JS task for timeline library.  Takes in files from src and outputs
-// to dist.  Uses JSHint, JSCS
+// to dist.  Uses JSHint, JSCS, add header, ...
 gulp.task('js', function() {
   return gulp.src('src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'))
     .pipe(jshint.reporter('fail'))
     .pipe(jscs())
+    .pipe(header(banner, { pkg: pkg }))
     .pipe(gulp.dest('dist'));
 });
 
