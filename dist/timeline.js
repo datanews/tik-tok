c6cb2447ba8fdf42b9b0f060a9f6b39b00d145df
Alan Palazzolo <apalazzolo@minnpost.com>
2015-04-27 17:05:06 -0500

    Removing jQuery dependency and adding moment and restructuring tests and creating basic timeline class.

--- dist/timeline.js
+++ dist/timeline.js
@@ -10,35 +10,90 @@
 
 // Wrap so that we can support different module loaders
 (function(root, factory) {
-  // AMD
-  if (typeof define === 'function' && define.amd) {
-    define(['underscore', 'jquery', 'exports'], function(_, $, exports) {
-      // Export global even in AMD case in case this script is loaded
-      // with others that may still expect a global Backbone.
-      root.Timeline = factory(root, exports, _, $);
-    });
+  // Common JS (i.e. browserify) or Node.js environment
+  if (typeof module !== 'undefined' && module.exports && typeof require === 'function') {
+    module.exports = factory(require('underscore'), require('moment'));
   }
-  else if (typeof exports !== 'undefined') {
-    // Node.js or CommonJS.
-    factory(root, exports, require('underscore'), require('jquery'));
+  else if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(['underscore', 'moment'], factory);
   }
   else {
     // Brower global
-    root.Timeline = factory(root, {}, root._, root.jQuery);
+    root.Timeline = factory(root._, root.moment);
   }
-
-})(this, function(root, Timeline, _, $) {
-  // Timeline should come in as an object
-  Timeline = Timeline || {};
-
+})(typeof window !== 'undefined' ? window : this, function(_, moment) {
   // Check depdencies
   if (typeof _ === 'undefined') {
     throw new Error('Underscore is a necessary dependency of Timeline.');
   }
 
-  if (typeof $ === 'undefined') {
-    throw new Error('jQuery is a necessary dependency of Timeline.');
+  if (typeof moment === 'undefined') {
+    throw new Error('Moment is a necessary dependency of Timeline.');
   }
+
+  // Default options
+  var defaultOptions = {
+    // Date formates used by moment
+    dateFormats: ['MMM DD, YYYY', 'MM/DD/YYYY', 'M/D/YYYY', 'DD MMM YYYY'],
+
+    // Column mapping in format { needed: provided }
+    columnMapping: {
+      date: 'date',
+      title: 'title',
+      body: 'body'
+    }
+  };
+
+  // Constructior
+  var Timeline = function(options) {
+    var _this = this;
+    this.options = _.extend({}, defaultOptions, options || {});
+
+    // Enusre there is data
+    if (!_.isArray(this.options.events)) {
+      throw new Error('"events" data was not provided as an array.');
+    }
+
+    // Ensure there is column mapping
+    if (!_.isObject(this.options.columnMapping)) {
+      throw new Error('"columnMapping" was not provided as an object.');
+    }
+
+    // Map columns and attach events to object for easier access
+    this.events = this.mapColumns(this.options.events, this.options.columnMapping);
+
+    // Parse out dates
+    this.events = _.map(this.events, function(e) {
+      e.date = moment(e.date, _this.options.dateFormats);
+    });
+  };
+
+  // Add methods
+  _.extend(Timeline.prototype, {
+    // Main renderer
+    render: function() {
+      console.log(this);
+    },
+
+    // Map columns
+    mapColumns: function(events, mapping) {
+      // Go through each event, clone, change mappings, and remove old
+      return _.map(events, function(e) {
+        var n = _.clone(e);
+
+        // Find a mapping
+        _.each(mapping, function(m, mi) {
+          if (!_.isUndefined(e[m])) {
+            n[mi] = _.clone(e[m]);
+            delete n[m];
+          }
+        });
+
+        return n;
+      });
+    }
+  });
 
   return Timeline;
 });
--- tests/index.js
+++ tests/index.js
@@ -8,17 +8,60 @@
 /* global describe:false, it:false */
 'use strict';
 
-// Dependencies
+// Dependencies.  See the following for assert reference
+// https://nodejs.org/api/assert.html
 var assert = require('assert');
+var Timeline = require('../dist/timeline');
 
-// Basic timeline parts
-describe('Basic Timeline', function() {
-  var T = require('../dist/timeline');
+// Tests for Timeline object
+describe('Timeline', function() {
 
-  describe('#object', function() {
-    it('should be an object', function() {
-      assert.equal(true, (typeof T !== 'undefined'));
-      assert.equal(true, (typeof T === 'object'));
+  // Timeline object itself
+  describe('#class', function() {
+    it('should be a function', function() {
+      assert.equal(true, (typeof Timeline !== 'undefined'));
+      assert.equal(true, (typeof Timeline === 'function'));
+    });
+  });
+
+  // Constructor
+  describe('#constructor', function() {
+    it('should throw an error without any events', function() {
+      assert.throws(function() {
+        var t = new Timeline();
+        t = {};
+      });
+    });
+  });
+
+  // Metho column mapping
+  describe('#mapColumns', function() {
+    it('should map columns', function() {
+      var events = [
+        {
+          'this is a date': '2014-05-01',
+          'this is out title': 'Title!',
+          'and our body is here': 'Over here!'
+        }
+      ];
+      var expected = [
+        {
+          date: '2014-05-01',
+          title: 'Title!',
+          body: 'Over here!'
+        }
+      ];
+      var columnMapping = {
+        date: 'this is a date',
+        title: 'this is out title',
+        body: 'and our body is here'
+      };
+      var t = new Timeline({
+        events: events,
+        columnMapping: columnMapping
+      });
+
+      assert.deepEqual(t.mapColumns(events, columnMapping), expected);
     });
   });
 });
