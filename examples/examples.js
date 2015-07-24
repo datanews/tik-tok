c0cf2d4886625ba36ac0ecdcc38b88765a808393
Alan Palazzolo <drupal@alanpalazzolo.com>
2015-07-24 17:12:40 -0500

    Replacement.

--- examples/examples.js
+++ examples/examples.js
@@ -1,4 +1,4 @@
-/* global hljs:false */
+/* global hljs:false, Egg:false */
 
 /**
  * JS specific to the examples.
@@ -9,4 +9,18 @@
 
   // Load highlightJS
   hljs.initHighlightingOnLoad();
+
+  // Egg$ all around
+  var egg = new Egg();
+  var tikTok = true;
+  egg.addCode([52, 52, 52], function() {
+    if (tikTok) {
+      document.getElementById('tik-tok-image').src = 'examples/images/tik-tok-replacement.jpg';
+    }
+    else {
+      document.getElementById('tik-tok-image').src = 'examples/images/tik-tok-medium.png';
+    }
+
+    tikTok = !tikTok;
+  }).listen();
 })();
