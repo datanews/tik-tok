d89db7f10ceb4188dec7e34795b6b9d758cc75ce
Alan Palazzolo <github@zzolo.org>
2015-07-31 17:26:15 -0500

    Some small updates.

--- examples/examples.js
+++ examples/examples.js
@@ -41,14 +41,53 @@
   // Egg$ all around
   var egg = new Egg();
   var tikTok = true;
+  var orig = {};
   egg.addCode([52, 52, 52], function() {
     if (tikTok) {
-      $('#tik-tok-image').attr('src', examplesPath + 'images/tik-tok-replacement.jpg');
+      $('#tik-tok-image').attr('src', examplesPath + 'images/tik-tok-replacement.png');
       $('.menu ul li img').attr('src', examplesPath + 'images/tik-tok-head-100-replacement.png');
+      $('.tik-tok-container, .tik-tok-container .tt-spine-bottom').css({
+        'background-image': 'url(' + examplesPath + 'images/tik-tok-bg-replacement.jpg)',
+        'backgroun-position': 'top left'
+      });
+      $('.tik-tok-container .tt-entry').css('background');
+      $('.tik-tok-container .tt-entry').css('background', 'rgba(255, 255, 255, 0.9)');
+      $('*').each(function() {
+        var $this = $(this);
+        if (!$this.html().match(/<|>/g)) {
+          $this.html($this.html().replace(/s/gi, '$'));
+        }
+      });
+
+      $('.tik-tok-container img').each(function() {
+        $(this).data('orig-src', $(this).attr('src'));
+        $(this).attr('src', examplesPath + 'images/tik-tok-main-replacement-01.jpg');
+      });
+
+      $('.tik-tok-container iframe').each(function() {
+        $(this).data('orig-src', $(this).attr('src'));
+        $(this).attr('src', 'https://www.youtube.com/embed/iP6XpLQM2Cs');
+      });
     }
     else {
       $('#tik-tok-image').attr('src', examplesPath + 'images/tik-tok-medium.png');
       $('.menu ul li img').attr('src', examplesPath + 'images/tik-tok-head-right-grey-100.png');
+      $('.tik-tok-container, .tik-tok-container .tt-spine-bottom').css('background-image', 'none');
+      $('.tik-tok-container .tt-entry').css('background', orig.entryBG);
+      $('*').each(function() {
+        var $this = $(this);
+        if (!$this.html().match(/<|>/g)) {
+          $this.html($this.html().replace(/\$/gi, 's'));
+        }
+      });
+
+      $('.tik-tok-container img').each(function() {
+        $(this).attr('src', $(this).data('orig-src'));
+      });
+
+      $('.tik-tok-container iframe').each(function() {
+        $(this).attr('src', $(this).data('orig-src'));
+      });
     }
 
     tikTok = !tikTok;
